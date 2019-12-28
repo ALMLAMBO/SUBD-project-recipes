440450ae953de2afd722bc58947e0ad9f04c70a3
ALMLAMBO <aleksm007148@gmail.com>
2019-12-29 00:22:29 +0200

    Add class for configuring connection to cloud sql

---
+++ RecipesSite/ConnectionConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace RecipesSite {
+	public class ConnectionConfig {
+		private MySqlConnectionStringBuilder csb;
+
+		public ConnectionConfig() {
+			csb = new MySqlConnectionStringBuilder() {
+				Server = "35.238.96.185",
+				UserID = "root",
+				Password = "}ferrari{",
+				Database = "recipessite",
+				SslMode = MySqlSslMode.None
+			};
+		}
+
+		public string getVersion() {
+			string version = "";
+
+			using (MySqlConnection conn = 
+				new MySqlConnection(csb.ConnectionString)) {
+
+				conn.Open();
+
+				string sql = "select version();";
+				MySqlCommand command = new MySqlCommand(sql, conn);
+				version = command.ExecuteScalar().ToString();
+			}
+
+			return version;
+		}
+	}
+}
