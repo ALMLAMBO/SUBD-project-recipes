4bff0d28153bcee1ded2d5c436b0b66fc2442de2
ALMLAMBO <aleksm007148@gmail.com>
2020-01-15 23:35:40 +0200

    Add binding model for creating menus

---
+++ RecipesSite.CommonModels/BindingModels/Menus/AddMenuBindingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.CommonModels.BindingModels.Menus {
+	public class AddMenuBindingModel {
+		[Required]
+		public string MenuName { get; set; }
+
+		[Required]
+		public DateTime CreatedAt { get; set; }
+	}
+}
