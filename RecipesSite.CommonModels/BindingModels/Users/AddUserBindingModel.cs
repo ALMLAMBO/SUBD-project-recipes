136e4fafa68f9d3146870e0d94efb8d7aabd7fa8
ALMLAMBO <aleksm007148@gmail.com>
2020-01-15 23:36:28 +0200

    Add binding model for creating users

---
+++ RecipesSite.CommonModels/BindingModels/Users/AddUserBindingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.CommonModels.BindingModels.Users {
+	public class AddUserBindingModel {
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
