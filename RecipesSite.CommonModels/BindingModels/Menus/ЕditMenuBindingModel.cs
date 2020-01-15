a2be1669b8f1e70c3f0694401a2ed7293c37b56a
ALMLAMBO <aleksm007148@gmail.com>
2020-01-15 23:44:52 +0200

    Add binding model for editing menu

---
+++ RecipesSite.CommonModels/BindingModels/Menus/ЕditMenuBindingModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipesSite.CommonModels.BindingModels.Menus {
+	public class ЕditMenuBindingModel {
+		public string MenuName { get; set; }
+	}
+}
