a17c0f771f95f2314707089dd76797647fdcc43c
ALMLAMBO <aleksm007148@gmail.com>
2020-01-15 23:35:13 +0200

    Add binding model for adding ingredients

---
+++ RecipesSite.CommonModels/BindingModels/Ingredients/AddIngredientBindingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.CommonModels.BindingModels.Ingredients {
+	public class AddIngredientBindingModel {
+		[Required]
+		public string IngredientName { get; set; }
+
+		[Required]
+		public int Kcal { get; set; }
+	}
+}
