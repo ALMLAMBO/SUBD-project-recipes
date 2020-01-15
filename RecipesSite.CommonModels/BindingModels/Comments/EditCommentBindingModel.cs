6c8fb669635ce4736e8892fd375f0abd8ff9064d
ALMLAMBO <aleksm007148@gmail.com>
2020-01-15 23:44:27 +0200

    Add binding model for editing comments

---
+++ RecipesSite.CommonModels/BindingModels/Comments/EditCommentBindingModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipesSite.CommonModels.BindingModels.Comments {
+	public class EditCommentBindingModel {
+		public string Content { get; set; }
+	}
+}
