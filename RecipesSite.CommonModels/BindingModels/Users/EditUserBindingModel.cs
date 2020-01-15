9e84c7eb1ad799d34d2954a8960884c891ad8356
ALMLAMBO <aleksm007148@gmail.com>
2020-01-15 23:45:07 +0200

    Add binding model for editing user

---
+++ RecipesSite.CommonModels/BindingModels/Users/EditUserBindingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.CommonModels.BindingModels.Users {
+	public class EditUserBindingModel {
+		public string Username { get; set; }
+
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
