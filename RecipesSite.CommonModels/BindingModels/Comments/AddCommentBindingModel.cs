d3098e730edd00a15caf554d1952f1f7f6859bfd
ALMLAMBO <aleksm007148@gmail.com>
2020-01-15 23:34:51 +0200

    Add binding model for adding comments

---
+++ RecipesSite.CommonModels/BindingModels/Comments/AddCommentBindingModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.CommonModels.BindingModels.Comments {
+	public class AddCommentBindingModel {
+		[Required]
+		public string Content { get; set; }
+	}
+}
