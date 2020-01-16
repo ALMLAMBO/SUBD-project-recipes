5e40a57ae4a238c75ce0cd7fc930972bd13325fb
ALMLAMBO <aleksm007148@gmail.com>
2020-01-16 10:46:52 +0200

    Add model for recipe, which communicates with database

---
+++ RecipesSite.Models/Recipe/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.Models.Recipe {
+	public class Recipe {
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public string RecipeName { get; set; }
+
+		[Required]
+		public string RecipeCategory { get; set; }
+
+		[Required]
+		public string RecipeImageLink { get; set; }
+	}
+}
