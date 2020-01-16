ef611fa828a16509a7751cac5447fbe281921d18
ALMLAMBO <aleksm007148@gmail.com>
2020-01-16 10:46:12 +0200

    Add model for ingredient, which communicates with database

---
+++ RecipesSite.Models/Ingredient/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.Models.Ingredient {
+	public class Ingredient {
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public string IngredientName { get; set; }
+
+		[Required]
+		public int Kcal { get; set; }
+	}
+}
