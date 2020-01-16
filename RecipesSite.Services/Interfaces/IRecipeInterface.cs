d6931ed4a1f03ef3e5887f160f32b0bcff7b136c
ALMLAMBO <aleksm007148@gmail.com>
2020-01-16 10:44:02 +0200

    Add interface for recipes

---
+++ RecipesSite.Services/Interfaces/IRecipeInterface.cs
@@ -0,0 +1,21 @@
+﻿using RecipesSite.Models.Ingredient;
+using RecipesSite.Models.Recipe;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipesSite.Services.Interfaces {
+	public interface IRecipeInterface {
+		public Dictionary<int, List<Recipe>> GetAllRecipes();
+
+		public Recipe GetRecipeById(int id);
+
+		public List<Recipe> GetRecipesByCategory(string catName);
+
+		public void UpdateRecipe(int id);
+
+		public void DeleteRecipe(int id);
+
+		public List<Ingredient> GetAllIngredientsForRecipe(int id);
+	}
+}
