e1afa20d8eabe727314174d87165ff56e52f1057
ALMLAMBO <aleksm007148@gmail.com>
2020-01-25 22:23:08 +0200

    Add model for comment

---
+++ RecipesSite.Models/Comment/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RecipesSite.Models.Comment {
+	public class Comment {
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public string Content { get; set; }
+	}
+}
