96237e3e6ea3fc6a92c24001b80138e0fe60b640
ALMLAMBO <aleksm007148@gmail.com>
2020-01-25 22:37:09 +0200

    Reorder using statements and add one more param for update reicpe method

--- RecipesSite.Services/Interfaces/IRecipeInterface.cs
+++ RecipesSite.Services/Interfaces/IRecipeInterface.cs
@@ -1,20 +1,27 @@
-﻿using RecipesSite.Models.Ingredient;
-using RecipesSite.Models.Recipe;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Text;
+using System.Collections.Generic;
+using RecipesSite.Models.Recipe;
+using RecipesSite.Models.Comment;
+using RecipesSite.Models.Ingredient;
+using RecipesSite.CommonModels.BindingModels.Recipes;
 
 namespace RecipesSite.Services.Interfaces {
 	public interface IRecipeInterface {
+		public void AddRecipe(AddRecipeBindingModel recipe);
+		
+		public void UpdateRecipe(int id, EditRecipeBindingModel recipe);
+
+		public void DeleteRecipe(int id);
+
 		public Dictionary<int, List<Recipe>> GetAllRecipes();
 
 		public Recipe GetRecipeById(int id);
 
 		public List<Recipe> GetRecipesByCategory(string catName);
 
-		public void UpdateRecipe(int id);
+		public List<Comment> GetAllCommentsForRecipe(int id);
 
-		public void DeleteRecipe(int id);
 
 		public List<Ingredient> GetAllIngredientsForRecipe(int id);
 	}
