af3960f0cc2c7bd22dd45a6450a94c75e6c2ad37
ALMLAMBO <aleksm007148@gmail.com>
2020-01-25 22:37:52 +0200

    Reorder methods

--- RecipesSite.Services/Interfaces/IRecipeInterface.cs
+++ RecipesSite.Services/Interfaces/IRecipeInterface.cs
@@ -9,19 +9,18 @@ using RecipesSite.CommonModels.BindingModels.Recipes;
 namespace RecipesSite.Services.Interfaces {
 	public interface IRecipeInterface {
 		public void AddRecipe(AddRecipeBindingModel recipe);
+
+		public Dictionary<int, List<Recipe>> GetAllRecipes();
 		
 		public void UpdateRecipe(int id, EditRecipeBindingModel recipe);
 
 		public void DeleteRecipe(int id);
-
-		public Dictionary<int, List<Recipe>> GetAllRecipes();
 
 		public Recipe GetRecipeById(int id);
 
 		public List<Recipe> GetRecipesByCategory(string catName);
 
 		public List<Comment> GetAllCommentsForRecipe(int id);
-
 
 		public List<Ingredient> GetAllIngredientsForRecipe(int id);
 	}
