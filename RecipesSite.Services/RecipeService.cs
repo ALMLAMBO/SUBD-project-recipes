346957bda1d047144ebd589293c3ba5370f7826b
Martin Petrov <martinpetrov2000mp@gmail.com>
2020-01-30 15:16:49 +0200

    Implement method for adding recipe

--- RecipesSite.Services/RecipeService.cs
+++ RecipesSite.Services/RecipeService.cs
@@ -10,13 +10,96 @@ using RecipesSite.CommonModels.BindingModels.Recipes;
 
 namespace RecipesSite.Services {
 	public class RecipeService : IRecipeService {
-
 		/// <summary>
 		/// Adds a recipe to the database
 		/// </summary>
 		/// <param name="recipe">Recipe to add</param>
 		public void AddRecipe(AddRecipeBindingModel recipe) {
-			
+			ConnectionConfig connection = new ConnectionConfig();
+			MySqlConnection mySqlConnection = connection
+				.GetMySqlConnection();
+
+			MySqlTransaction mySqlTransaction = mySqlConnection
+				.BeginTransaction();
+
+			try {
+				MySqlCommand mySqlCommand = new MySqlCommand();
+				mySqlCommand.Connection = mySqlConnection;
+				mySqlCommand.CommandText = "select max(Id) as LastId from Recipes;";
+				MySqlDataReader reader = mySqlCommand.ExecuteReader();
+				int lastId = 0;
+
+				while(reader.Read()) {
+					lastId = reader.GetInt32("LastId");
+				}
+
+				Recipe recipeToAdd = new Recipe() {
+					Id = lastId,
+					RecipeName = recipe.RecipeName,
+					RecipeCategory = recipe.RecipeCategory,
+					RecipeIngrediets = recipe.Ingredients,
+					WayOfCooking = recipe.WayOfCooking
+				};
+
+				mySqlCommand.CommandText = "insert into Recipes value " +
+					"(@recipeCategory, @recipeName, @wayOfCooking);";
+
+				mySqlCommand.Parameters.AddWithValue(
+					"@recipeCategory", recipeToAdd.RecipeCategory);
+
+				mySqlCommand.Parameters.AddWithValue(
+					"@recipeName", recipeToAdd.RecipeName);
+
+				mySqlCommand.Parameters.AddWithValue(
+					"@wayOfCooking", recipeToAdd.WayOfCooking);
+
+				mySqlCommand.ExecuteNonQuery();
+
+				mySqlCommand.CommandText = "insert into Ingredients(IngredientName, Kcal)" +
+					" value (@ingredientName, @kcal);";
+
+				string sql = "select * from Ingredients where id = @id;";
+				MySqlCommand command = new MySqlCommand
+					(sql, mySqlConnection, mySqlTransaction);
+
+
+				foreach (var item in recipeToAdd.RecipeIngrediets) {
+					command.Parameters.AddWithValue("@id", item.Id);
+					MySqlDataReader r = command.ExecuteReader();
+					if(r.HasRows) {
+						while(r.Read()) {
+							int ingredientId = r.GetInt32("Id");
+
+							command.CommandText = "insert into RecipeIngredients" +
+								"value (@recipeId, @ingredientId);";
+
+							command.Parameters.AddWithValue("@recipeId", recipeToAdd.Id);
+							command.Parameters.AddWithValue("@ingredientId", item.Id);
+
+							command.ExecuteNonQuery();
+							command.Parameters.Clear();
+						}
+					}
+					else {
+						mySqlCommand.CommandText = "insert into Ingredients" +
+							"(IngredientName, Kcal) value (@ingredientName, @kcal);";
+
+						mySqlCommand.Parameters.AddWithValue(
+							"@ingredientName", item.IngredientName);
+
+						mySqlCommand.Parameters.AddWithValue(
+							"@kcal", item.Kcal);
+
+						mySqlCommand.ExecuteNonQuery();
+						mySqlCommand.Parameters.Clear();
+					}
+				}
+
+				mySqlTransaction.Commit();
+			} 
+			catch {
+				mySqlTransaction.Rollback();
+			}
 		}
 
 		/// <summary>
